40d9b3e497e683cc684a41c971fd5587d5c6d4b1
Shukria <shukriasul5@gmail.com>
2024-11-14 21:58:04 +0430

    Updated the starting quote content and style as well as corrected the spelling mistakes in the Persian translation file.

--- src/Components/SartingQuote.jsx
+++ src/Components/SartingQuote.jsx
@@ -3,9 +3,15 @@ import { useTranslation } from 'react-i18next'
 function SartingQuote() {
     const {t} = useTranslation();
   return (
-     <div className="container-fluid">
-        <p>{t('startingquote')}</p>         
-          <p>{t('prophetQuote')}</p>
+     <div className="container-fluid starting-quote-con  d-flex flex-column">
+      <div className="container  rounded-3 col-lg-6 mt-5 fw-bolder p-3  text-center first-quote"
+        style={{fontSize:"1.4rem"}}
+      >
+      <p>{t('startingquote')}</p>  
+      </div>
+           <div className="container p-3 mt-4 fw-bolder text-center rounded-3"   style={{fontSize:"1.4rem"}}>
+          <p>{t('prophetQuote')}</p>                                                 
+           </div>
      </div>
   )
 }
