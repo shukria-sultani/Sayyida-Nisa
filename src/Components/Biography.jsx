2cd1ffbe4cc3f23de6e5b1bc1594c4fcef0dfd6c
Shukria <shukriasul5@gmail.com>
2024-11-30 21:26:34 +0430

    Updated the biography component design.

--- src/Components/Biography.jsx
+++ src/Components/Biography.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import BiographySections from './BiographySections'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import BiographySections from "./BiographySections";
+import { useTranslation } from "react-i18next";
+
 function Biography() {
-  const {t} = useTranslation();
+  const { t } = useTranslation(); 
   return (
-     <div className="container">
-          <div className="container mt-5">
-            <h3>{t("title")}</h3>
-            <BiographySections
-              heading={t("earlyLife")}
-              bodyText={t("earlyLifeContent")}
-            />
-                   
-            
-            
-          </div>
-     </div>
-  )
+    <div className="container-fluid mt-5 pt-5">
+      <h3 className="text-center fw-bolder">{t("title")}</h3>
+      <div className="row">
+        <div className="col-12">
+          <BiographySections
+            heading={t("earlyLife")}
+            bodyText={t("earlyLifeContent")}
+            sources={t("earlyLifeSources")}
+          />
+        </div>
+        <div className="col-12">
+          <BiographySections
+            heading={t("earlyLife")}
+            bodyText={t("earlyLifeContent")}
+            sources={t("earlyLifeSources")}
+          />   
+        </div>
+        <div className="col-12">
+          <BiographySections
+            heading={t("earlyLife")}
+            bodyText={t("earlyLifeContent")}
+            sources={t("earlyLifeSources")}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Biography
+export default Biography;
--- src/Components/BiographySections.jsx
+++ src/Components/BiographySections.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function BiographySections({heading, bodyText}) {
+function BiographySections({ heading, bodyText, sources }) {
   return (
-    <div className="container">
-         <h4>{heading}</h4>
-         <p>{bodyText}</p>
+    <div className="container-fluid p-sm-2  bio-section mt-5 p-lg-5 ">
+      <h2 className='fw-bolder p-3 text-success text-center'>{heading}</h2>
+       <hr className='text-warning' />
+      <p className='pt-2 text-center lh-lg'>{bodyText}</p>
+      <div dangerouslySetInnerHTML={{ __html: sources }} />
     </div>
-  )
+  );
 }
 
-export default BiographySections
+export default BiographySections;
