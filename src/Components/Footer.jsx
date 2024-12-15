c0acdb65d8452cd2f95892758a2fb2f759938633
Shukria <shukriasul5@gmail.com>
2024-12-15 15:00:59 +0430

    Replaced the regular anchor tags with Link component in Footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-
+import {Link} from "react-router-dom"
 function Footer() {
   const { t } = useTranslation();
   return (
@@ -9,19 +9,19 @@ function Footer() {
       style={{ backgroundColor: "#007A33" }}
     >
       <div className=" text-center fw-bolder " style={{ fontSize: "1.5em" }}>
-        {t("salawat")}
+        {t("salawat")}s
       </div>
       <div className="container pt-5">
           <h4 className="fw-bolder">{t("quickLinks")}</h4>
         <ul className="list-unstyled " style={{fontSize: "1.1em"}}>
-          <li className="text-decoration-none">
-            <a href="" className="text-warning"> {t('AboutFatime')}</a>
+          <li className="pt-2">
+            <Link to={'/'} className="text-warning text-decoration-none"> {t('AboutFatime')}</Link>
           </li>
           <li >
-            <a href="" className="text-warning"> {t('LifeLessons')}</a>
+            <Link to={"/lifelessons"} className="text-warning text-decoration-none"> {t('LifeLessons')}</Link>
           </li>
           <li >
-            <a href="" className="text-warning"> {t('Resources')}</a>
+            <Link to={"/resources"} className="text-warning text-decoration-none"> {t('Resources')}</Link>
           </li>
         </ul>
          <div className="container fw-bolder text-center" style={{fontSize: "2em"}}>
