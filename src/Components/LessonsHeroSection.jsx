574f12f948ae42b32a48cf9f0f16536e40691052
Shukria <shukriasul5@gmail.com>
2024-12-16 19:59:18 +0430

    Created the life lessons page hero section, updated its content in the translation files and updated its styling in the  CSS file.

--- src/Components/LessonsHeroSection.jsx
+++ src/Components/LessonsHeroSection.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useTranslation } from "react-i18next";
 
 function LessonsHeroSection() {
+  const {t} = useTranslation()
   return (
-    <div>LessonsHeroSection</div>
+    <div className='container-fluid life-lessson-hero '>
+        <div className="container p-5">
+          <p className='text-warning pt-5 ms-lg-5 mt-5 ps-lg-5 fw-bolder display-6 text-center'
+             dangerouslySetInnerHTML={{ __html: t("lifeLessonsHero") }}
+          >
+          </p>
+        </div>
+    </div>
   )
 }
 
