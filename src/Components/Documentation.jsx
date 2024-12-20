a26ead65f63fb914e66eaa79c53e044688aa2da2
Shukria <shukriasul5@gmail.com>
2024-12-20 23:18:03 +0430

    Added the Documentation section on the Resources pages.

---
+++ src/Components/Documentation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Documentation() {
+  return (
+ <div className="d-flex mt-5 pt-5 justify-content-center">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/xfiv0WUQe1c?si=m9EYFN_4sew9YKrm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+  )
+}
+
+export default Documentation
--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
 import ResourcesHero from '../Components/ResourcesHero'
+import Documentation from '../Components/Documentation'
 function Resources() {
   return (
     <>
     <Header />
     <ResourcesHero />
+    <Documentation />
     <Footer />
     
     </>
