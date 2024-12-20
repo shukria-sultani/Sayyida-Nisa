fd7372f0ef8afe29f639dfd2885d2d76d0ef44cb
Shukria <shukriasul5@gmail.com>
2024-12-20 23:06:27 +0430

    Added the hero section on the Resources page with content and styling.

---
+++ src/Components/ResourcesHero.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import { useTranslation } from "react-i18next";
+import 'react-slideshow-image/dist/styles.css';
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  backgroundPosition: "center",
+  height: "90vh"
+};
+
+const ResourcesHero = () => {
+    const { t, i18n } = useTranslation(); // Get the current language
+    const currentLang = i18n.language; // Get the current language
+
+    const slideImages = [
+        {
+          url: '../public/images/fa.jpeg',
+          caption: t("resourceQuote1")
+        },
+        {
+          url: '../public/images/milad-fateme-8a.jpg',
+          caption: t("resourceQuote2")
+        },
+        {
+          url: '../public/images/f.jpeg',
+          caption: 'Slide 3'
+        },
+    ];
+
+    // Determine styles based on language
+    const textStyle = {
+      backgroundColor: "rgba(248, 248, 239, 0.9)",
+      direction: currentLang === 'fa' ? 'rtl' : 'ltr', // Set direction based on language
+      textAlign: currentLang === 'fa' ? 'right' : 'left', // Align text based on language
+      fontSize:  "30px"
+    };
+
+    return (
+      <div className="slide-container">
+        <Slide 
+          duration={2000} 
+          transitionDuration={1000} 
+        >
+          {slideImages.map((slideImage, index) => (
+            <div key={index} style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}>
+              <div className='col-lg-6 p-5 text-center fw-bolder text-warning' style={textStyle}> 
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    );
+}
+
+export default ResourcesHero;
--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
+import ResourcesHero from '../Components/ResourcesHero'
 function Resources() {
   return (
     <>
     <Header />
+    <ResourcesHero />
     <Footer />
     
     </>
