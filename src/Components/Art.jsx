d8983c61e08caca17ca1ab398cdceceffe305974
Shukria <shukriasul5@gmail.com>
2024-12-23 14:29:01 +0430

    Fix image paths in Art component.

--- src/Components/Art.jsx
+++ src/Components/Art.jsx
@@ -8,32 +8,32 @@ function Art() {
        <h1 className='text-center'>{t("artTitle")}</h1>
       <div className="d-flex flex-wrap text-center gap-4 p-5">
         <ArtSection 
-          image={"../public/images/hazratfatemeh008.jpg"} 
+          image={"/images/hazratfatemeh008.jpg"} 
           description={t("art1Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
         <ArtSection 
-          image={"../public/images/Katibeh(177)-Karbobala.com.jpg"} 
+          image={"/images/Katibeh(177)-Karbobala.com.jpg"} 
           description={t("art2Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
         <ArtSection 
-          image={"../public/images/Katibeh(13100)-Karbobala.jpg"} 
+          image={"/images/Katibeh(13100)-Karbobala.jpg"} 
           description={t("art3Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
         <ArtSection 
-          image={"../public/images/hazrat_fatemeh_075.jpg"} 
+          image={"/images/hazrat_fatemeh_075.jpg"} 
           description={t("art4Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
         <ArtSection 
-          image={"../public/images/Alah copy.jpg"} 
+          image={"/images/Alah copy.jpg"} 
           description={t("art5Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
         <ArtSection 
-          image={"../public/images/463616_-2.jpg"} 
+          image={"/images/463616_-2.jpg"} 
           description={t("art6Description")}
           source={"https://shiaarts.ir/gallery/%D9%81%D8%A7%D8%B7%D9%85%D8%A9-%D8%A7%D9%84%D8%B2%D9%87%D8%B1%D8%A7-%D8%B3-0"}
         />
