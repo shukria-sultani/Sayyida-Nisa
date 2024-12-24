31f2010824b436ec94b883249c149d5f8629cf30
Shukria <shukriasul5@gmail.com>
2024-12-24 11:04:30 +0430

    Fixed the a  tag target to blank.

--- src/Components/ArtSection.jsx
+++ src/Components/ArtSection.jsx
@@ -6,7 +6,7 @@ function ArtSection({image, description, source}) {
      <div className="container  d-flex flex-column flex-wrap align-items-center justify-content-center text-center col-12 pt-5">
           <img src={image}  max-height={"350px"}  max-weight={"500px"} className='col-12 col-lg-6'/>
            <div className='fw-bolder p-3  ' style={{fontSize: "20px"}}>{description}</div>
-           <button className='art-link rounded-2 col-lg-6 col-12  p-3 text-warning'><a href={source} target_blank>{t("linkToArt")}</a></button>
+           <button className='art-link rounded-2 col-lg-6 col-12  p-3 text-warning'><a href={source} target="_blank">{t("linkToArt")}</a></button>
      </div>
   )
 }
